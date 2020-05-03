705a469c7c633954d45f287658db8280e40273da
Samet Safkan <sametsafkan@gmail.com>
2020-05-03 10:21:57 +0300

    initial commit.

---
+++ src/main/java/com/sametsafkan/mssbreweryconfigserver/MssBreweryConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.sametsafkan.mssbreweryconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssBreweryConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MssBreweryConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sametsafkan/mssbreweryconfigserver/MssBreweryConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sametsafkan.mssbreweryconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssBreweryConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
